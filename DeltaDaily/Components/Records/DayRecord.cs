e9510c85f895de4677fa874ebd876733e701148f
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2025-10-13 07:42:33 -0300

    Reorganization

---
+++ DeltaDaily/Components/Records/DayRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace DeltaDaily.Components.Records;
+
+public sealed class DayRecord
+{
+    public string Id { get; set; } = default!;
+    public string Date { get; set; } = default!;
+    public bool Closed { get; set; }
+    public DateTime UpdatedAtUtc { get; set; }
+    public string Payload { get; set; } = "";
+}
